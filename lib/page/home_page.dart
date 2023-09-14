81681f122c295cc7010facfa2a3360a98cbf6f9e
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2023-09-14 11:41:54 -0300

    textfields + buttom

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +13,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text(
           "Calculadora IMC",
@@ -18,30 +21,89 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        margin: EdgeInsets.only(top: 20),
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                "Aqui você pode calcular e armazenar seu IMC",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 50),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 30),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Altura",
-                    hintText: "Digite sua altura.",
-                    border: OutlineInputBorder(),
-                    suffixIcon: Icon(
-                      Icons.height,
+        margin: const EdgeInsets.only(top: 20),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SvgPicture.asset('lib/assets/balanca.svg', height: 150),
+                const SizedBox(height: 30),
+                const Text(
+                  "Aqui você pode calcular e armazenar seu IMC",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                const Text("Prencha com seus dados:"),
+                const SizedBox(height: 40),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: "Altura",
+                      hintText: "Digite sua altura.",
+                      border: OutlineInputBorder(),
+                      suffixIcon: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 13, horizontal: 16),
+                        child: FaIcon(
+                          FontAwesomeIcons.rulerVertical,
+                          color: Colors.blueGrey,
+                          size: 20,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              )
-            ],
+                const SizedBox(height: 20),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: "Peso",
+                      hintText: "Digite seu peso.",
+                      border: OutlineInputBorder(),
+                      suffixIcon: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 13, horizontal: 10),
+                        child: FaIcon(
+                          FontAwesomeIcons.weightScale,
+                          color: Colors.blueGrey,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    width: double.infinity,
+                    height: 40,
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color.fromRGBO(105, 240, 174, 1))),
+                        child: const Text(
+                          "CALCULAR",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(2.0, 2.0),
+                                  blurRadius: 0.0,
+                                  color: Color.fromARGB(54, 0, 0, 0),
+                                )
+                              ]),
+                        )))
+              ],
+            ),
           ),
         ),
       ),
